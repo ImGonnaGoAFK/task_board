3d0734e5b3c745696a99a7502617750b3973073d
Kevin <kevhoang97@gmail.com>
2024-06-20 16:59:56 -0400

    added addCard function

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+    const taskCard = $('<div>').addClass ('card task-card draggable my-3').attr('data-task-id', task-id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.type);
+    const cardDueDate =$('<p>').addClass('card-text').text(task.cardDueDate)
+    const taskDeleteBtn = $('<button>').addClass('btn btn-danger delete').text('Delete').attr('data-task-id', task-id);
+    taskDeleteBtn.on('click', handleDeleteTask);
+
+    if (task.dueDate && project.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+        if (now.isSame(taskDueDate,'day')) {
+            taskCard.addClass('bg-warning text-white');
+        }
+        else if (now.isAfter(taskDueDate)) {
+            taskCard.addClass('bg-danger text-white');
+            taskDeleteBtn.addClass('border-light');
+        }
+    }
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+
+}
+
+// Todo: create a function to handle adding a new task
+function handleAddTask(event){
+
+}
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event){
+
+}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+
+});
