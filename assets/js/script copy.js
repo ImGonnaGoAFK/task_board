a1f4b4398935edfe4b846b39353a62ddd84f056b
Kevin <kevhoang97@gmail.com>
2024-06-21 19:02:55 -0400

    made columns droppable

--- assets/js/script copy.js
+++ assets/js/script copy.js
@@ -6,7 +6,6 @@ console.log(taskList)
 
 function saveTaskstoStorage (tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks));
-  renderTaskList()
 }
 
 function readTasksFromStorage() {
@@ -16,6 +15,7 @@ function readTasksFromStorage() {
     saveTaskstoStorage (tasks);
   }
   return tasks;
+  
 }
 
 // Todo: create a function to generate a unique task id
@@ -60,7 +60,7 @@ function renderTaskList() {
   const inProgressList = $('#in-progress-cards');
   inProgressList.empty();
 
-  const doneList = $('#done-cards');
+  const doneList = $('done-cards');
   doneList.empty();
 
   for (let task of tasks) {
@@ -187,32 +187,22 @@ function handleDeleteTask(event){
  })
 
  saveTaskstoStorage (tasks);
+ renderTaskList();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
  const tasks = readTasksFromStorage();
+ const taskId = ui.draggable[0].dataset.projectId;
+ const newStatus = event.target.id;
 
- var newStatus;
- if(event.target.id === "done-cards"){
-  newStatus = "done";
- }
- if(event.target.id === "in-progress-cards"){
-  newStatus = "in-progress";
- }
- if(event.target.id === "todo-cards"){
-  newStatus = "to-do";
- }
- //repeat that logic
- var draggedElement = event.originalEvent.target.offsetParent
- const taskId = draggedElement.getAttribute("data-task-id");
  for (let task of tasks) {
-  if (task.id === taskId) {
+  if (projectId === taskId) {
     task.status = newStatus;
   }
  }
-saveTaskstoStorage(tasks) 
+ localStorage.setItems('tasks', JSON.stringify(tasks));
+ renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -220,10 +210,6 @@ $(document).ready(function () {
   readTasksFromStorage ();
   renderTaskList ();
   handleAddTask();
-  $('.drag-target').droppable({
-    accept: '.draggable',
-    drop: handleDrop,
-  });
 });
 
 // function init(){
